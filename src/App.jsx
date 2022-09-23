4e6e31f5dc70ec006e38b823671c9142759f71e0
aleksmagnusson <aleks.magnusson@gmail.com>
2022-09-23 10:31:23 +0200

    Fixed Routing x Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
 // Import BrowserRouter
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages
-import Front from './components/Front'
+import Front from "./components/Front";
 import Home from "./components/Pages/Home";
 import About from "./components/Pages/About";
 import Contact from "./components/Pages/Contact";
 import Showcase from "./components/Pages/Showcase";
 import Navbar from "./components/Navbar";
 // Css
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Front />} />
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/showcase" component={Showcase} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/showcase" element={<Showcase />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -1,30 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 // import { BrowserRouter as Router, Route } from "react-router-dom"
-import frontVideo from '../assets/aleksEdit1.mp4'
-import '../components/front.css'
-import Navbar from './Navbar'
+import frontVideo from "../assets/aleksEdit1.mp4";
+import "../components/front.css";
 
 function Front() {
-
-    return (
-        <main>
-            <div className='frontpage'>
-                <div className='frontOverlay'>
-                    <video src={frontVideo} autoPlay loop muted />
-                    <div className='frontContent'>
-                        <h2>Aleks Magnusson</h2>
-                        <br />
-                        <h4>Photographer | content creator | frontend dev student</h4>
-                        <br />
-                        <div>
-                            <button as={Link} to="/showcase"> Dive into my adventure.</button>
-                            <button as={Link} to="/contact"> Contact me.</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <main>
+      <div className="frontpage">
+        <div className="frontOverlay">
+          <video src={frontVideo} autoPlay loop muted />
+          <div className="frontContent">
+            <h2>Aleks Magnusson</h2>
+            <br />
+            <h4>Photographer | content creator | frontend dev student</h4>
+            <br />
+            <div>
+              <button as={Link} to="/showcase">
+                {" "}
+                Dive into my adventure.
+              </button>
+              <button as={Link} to="/contact">
+                {" "}
+                Contact me.
+              </button>
             </div>
-        </main>
-    )
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Front;
